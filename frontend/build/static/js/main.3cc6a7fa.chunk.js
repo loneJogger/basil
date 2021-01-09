69ef4204c01a7145fc768d142d681dcebd9618bd
ATREMARC <atremarc@holycross.edu>
2021-01-09 18:14:03 -0500

    added frontend build

---
+++ frontend/build/static/js/main.3cc6a7fa.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[0],{10:function(n,t,e){"use strict";e.r(t);var r=e(0),c=e(1),o=e.n(c),d=e(3),s=e.n(d),i=(e(9),function(){return Object(r.jsx)("div",{children:"Pardon our dust!"})});s.a.render(Object(r.jsx)(o.a.StrictMode,{children:Object(r.jsx)(i,{})}),document.getElementById("root"))},9:function(n,t,e){}},[[10,1,2]]]);
+//# sourceMappingURL=main.3cc6a7fa.chunk.js.map
